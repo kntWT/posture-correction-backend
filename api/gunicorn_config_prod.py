33a8a823483bf843b7bc2e07ecb5cff4163e5ab8
kntWT <kento.wata0628@gmail.com>
2024-11-21 19:36:42 +0900

    feat(api): gunicorn導入

---
+++ api/gunicorn_config_prod.py
@@ -0,0 +1,29 @@
+# 実行するPythonがあるパス
+pythonpath = './'
+
+# ワーカー数
+workers = 4
+
+# ワーカーのクラス、*2 にあるようにUvicornWorkerを指定 (Uvicornがインストールされている必要がある)
+worker_class = 'uvicorn.workers.UvicornWorker'
+
+# IPアドレスとポート
+bind = '0.0.0.0:3000'
+
+# プロセスIDを保存するファイル名
+pidfile = 'prod.pid'
+
+# Pythonアプリに渡す環境変数
+raw_env = ['MODE=PROD']
+
+# デーモン化する場合はTrue
+daemon = True
+
+# エラーログ
+errorlog = './logs/error_log.txt'
+
+# プロセスの名前
+proc_name = 'posture_correction_api'
+
+# アクセスログ
+accesslog = './logs/access_log.txt'
