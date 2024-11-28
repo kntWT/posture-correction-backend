ddf3da928ee2924354ec53f6c69300888c5b4446
kntWT <kento.wata0628@gmail.com>
2024-11-28 16:58:13 +0900

    feat(api): fast API部分をappファイルに移動

---
+++ api/app.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, status, Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI()
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:4200",
+    "https://localhost:4200",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.exception_handler(RequestValidationError)
+async def handler(request:Request, exc:RequestValidationError):
+    print(exc)
+    return JSONResponse(content={}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
+@app.get("/")
+def get_hello_world():
+    return {"Hello": "World"}
+
+routers_list = []
+
+for router in routers_list:
+    app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=3000,
+        reload=True,
+        workers="auto"
+    )
