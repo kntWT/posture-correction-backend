d7c63fa3cf42a791519c218cc1fe22e9575d2207
kntWT <kento.wata0628@gmail.com>
2024-12-02 11:41:51 +0900

    refactor(api): ディレクトリ名を複数形に統一

--- api/configs/db.py
+++ api/configs/db.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import time
 
 from config.env import user, password, db_name, host
@@ -9,13 +11,13 @@ def connect_db(trial: int):
         return None, None, None
     try:
         engine = create_engine(f'mysql+pymysql://{user}:{password}@mysql:3306/{db_name}')
-        meta = MetaData()
-        conn = engine.connect()
-        return engine, meta, conn
+        SessionLocal = sessionmaker(bind=engine)
+        Base = declarative_base()
+        return engine, SessionLocal, Base
     except Exception as e:
         time.sleep(1)
         print(e)
         print(host)
         return connect_db(trial + 1)
     
-engine, meta, conn = connect_db(0)
+engine, SessionLocal, Base = connect_db(0)
