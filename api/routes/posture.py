074c6070a6942d86de358bc6309d2c64dcfe3c10
kntWT <kento.wata0628@gmail.com>
2024-12-12 19:14:46 +0900

    feat(api): openapiにエラーレスポンスを認識させるように

--- api/routes/posture.py
+++ api/routes/posture.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from schemas.posture import Posture, PostureCreate, PostureOnlysensor, PostureOnlyFace, PostureOnlyPosition, PostureOnlyFilename
 from schemas.user import User
+from schemas.http_exception import BadRequestException, UnauthorizedException, ForbiddenException, NotFoundException, error_responses
 import cruds.posture as crud
 from guards.auth import login_auth, admin_auth
 from configs.db import get_db
@@ -9,41 +10,43 @@ from configs.db import get_db
 posture = APIRouter(prefix="/posture", tags=["posture"])
 
 
-@posture.get("/", response_model=list[Posture])
+@posture.get("/", response_model=list[Posture], responses=error_responses([UnauthorizedException, ForbiddenException]))
 async def get_postures(db: Session = Depends(get_db), _admin: User = Depends(admin_auth)):
     return crud.get_postures(db)
 
 
-@posture.get("/{posture_id}", response_model=Posture)
+@posture.get("/{posture_id}", response_model=Posture, responses=error_responses([NotFoundException, UnauthorizedException, ForbiddenException]))
 async def get_posture_by_id(posture_id: int, db: Session = Depends(get_db), _login: User = Depends(login_auth)):
-    return crud.get_posture_by_id(db, posture_id)
+    p = crud.get_posture_by_id(db, posture_id)
+    if p is None:
+        raise NotFoundException("No posture found")
 
 
-@posture.get("/user/{user_id}", response_model=list[Posture])
+@posture.get("/user/{user_id}", response_model=list[Posture], responses=error_responses([UnauthorizedException]))
 async def get_posture_by_user_id(user_id: int, db: Session = Depends(get_db), _login: User = Depends(login_auth)):
     return crud.get_posture_by_user_id(db, user_id)
 
 
-@posture.post("/create", response_model=Posture)
+@posture.post("/create", response_model=Posture, responses=error_responses([UnauthorizedException]))
 async def create_posture(posture: PostureCreate, db: Session = Depends(get_db), _login: User = Depends(login_auth)):
     return crud.create_posture(db, posture)
 
 
-@posture.put("/filename", response_model=Posture)
-async def update_filename(posture: PostureOnlyFilename, db: Session = Depends(get_db)):
+@posture.put("/filename", response_model=Posture, responses=error_responses([UnauthorizedException, ForbiddenException]))
+async def update_filename(posture: PostureOnlyFilename, db: Session = Depends(get_db), _admin: User = Depends(admin_auth)):
     return crud.update_filename(db, posture)
 
 
-@posture.put("/sensor", response_model=Posture)
-async def update_sensor(posture: PostureOnlysensor, db: Session = Depends(get_db)):
+@posture.put("/sensor", response_model=Posture, responses=error_responses([UnauthorizedException, ForbiddenException]))
+async def update_sensor(posture: PostureOnlysensor, db: Session = Depends(get_db), _admin: User = Depends(admin_auth)):
     return crud.update_sensor(db, posture)
 
 
-@posture.put("/face", response_model=Posture)
-async def update_face(posture: PostureOnlyFace, db: Session = Depends(get_db)):
+@posture.put("/face", response_model=Posture, responses=error_responses([UnauthorizedException, ForbiddenException]))
+async def update_face(posture: PostureOnlyFace, db: Session = Depends(get_db), _admin: User = Depends(admin_auth)):
     return crud.update_face(db, posture)
 
 
-@posture.put("/position", response_model=Posture)
+@posture.put("/position", response_model=Posture, responses=error_responses([UnauthorizedException, ForbiddenException]))
 async def update_position(posture: PostureOnlyPosition, db: Session = Depends(get_db), _admin: User = Depends(admin_auth)):
     return crud.update_position(db, posture)
