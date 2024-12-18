35bb70bdb8b092474fee3dcfe603bbacc5b554f8
kntWT <kento.wata0628@gmail.com>
2024-12-18 14:37:55 +0900

    fix: typo

--- api/utils/jwt.py
+++ api/utils/jwt.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from typing import Optional
 from utils.jst import JST
 from configs.env import secret_key
+from schemas.http_exception import TokenExpiredException, UnauthorizedException
 
 ALGORITHM = "EdDSA"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60  # トークンの有効期限（分）
@@ -16,15 +17,15 @@ def generate_token(data: dict, expires_delta: Optional[timedelta] = None) -> str
         expire = datetime.now(
             JST) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=ALGORITHM)
     return encoded_jwt
 
 
 def decode_token(token: str) -> dict:
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, secret_key, algorithms=[ALGORITHM])
         return payload
     except jwt.ExpiredSignatureError:
-        raise ValueError("Token expired")
+        raise TokenExpiredException("Token expired")
     except jwt.InvalidTokenError:
-        raise ValueError("Invalid token")
+        raise UnauthorizedException("Invalid token")
