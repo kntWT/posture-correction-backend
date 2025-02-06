3a0ee6f2bc32ae93b0d7413caa7a244e3c7a8c7e
kntWT <kento.wata0628@gmail.com>
2025-02-06 13:22:11 +0900

    feat(api): 首の角度推定のルーティング

--- api/routes/posture.py
+++ api/routes/posture.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, File, UploadFile, Form, Body
 from sqlalchemy.orm import Session
-from schemas.posture import Posture, PostureCreate, PostureOnlysensor, PostureOnlyFace, PostureOnlyPosition, PostureOnlyFilename
+from schemas.posture import Posture, PostureCreate, PostureOnlySensor, PostureOnlyFace, PostureOnlyPosition, PostureOnlyFilename
 from schemas.user import User
 from schemas.http_exception import BadRequestException, UnauthorizedException, ForbiddenException, NotFoundException, error_responses
+import cv2
 import cruds.posture as crud
+from configs.env import get_db, image_dir
+from estimators.features.estimate import estimate_from_image as estimate_feature_from_image
+from estimators.estimate import estimate_from_image, estimate_from_features
 from guards.auth import login_auth, admin_auth
-from configs.db import get_db
+from helpers.save_file import save_file
 
 posture = APIRouter(prefix="/posture", tags=["posture"])
 
@@ -32,13 +36,40 @@ async def create_posture(posture: PostureCreate, db: Session = Depends(get_db), 
     return crud.create_posture(db, posture)
 
 
+@posture.post("/estimate", response_model=Posture, responses=error_responses([UnauthorizedException, BadRequestException]))
+async def estimate_posture(file: UploadFile = File(...), sensors: PostureOnlySensor = Body(...), db: Session = Depends(get_db), user: User = Depends(login_auth)):
+    file_path = save_file(file.file, image_dir,
+                          f"original/{user.id}", file.filename)
+    if file_path is None:
+        raise BadRequestException("Failed to upload file")
+    img = cv2.imread(file_path)
+    face_feature, head_feature = await estimate_feature_from_image(img, user.id, file.filename)
+    if face_feature is None or head_feature is None:
+        raise BadRequestException("Failed to estimate posture")
+    neck_angle = estimate_from_features({**face_feature, **head_feature})
+    return crud.create_posture(db, PostureCreate(user_id=user.id, file_name=file.filename, neck_angle=neck_angle, **face_feature, **head_feature))
+
+
+@posture.post("/estimate/feature", response_model=Posture, responses=error_responses([UnauthorizedException, BadRequestException]))
+async def estimate_feature(file: UploadFile = File(...), db: Session = Depends(get_db), user: User = Depends(login_auth)):
+    file_path = save_file(file.file, image_dir,
+                          f"original/{user.id}", file.filename)
+    if file_path is None:
+        raise BadRequestException("Failed to upload file")
+    img = cv2.imread(file_path)
+    face_feature, head_feature = await estimate_feature_from_image(img, user.id, file.filename)
+    if face_feature is None or head_feature is None:
+        raise BadRequestException("Failed to estimate posture")
+    return crud.create_posture(db, PostureCreate(user_id=user.id, file_name=file.filename, **face_feature, **head_feature))
+
+
 @posture.put("/filename", response_model=Posture, responses=error_responses([UnauthorizedException, ForbiddenException]))
 async def update_filename(posture: PostureOnlyFilename, db: Session = Depends(get_db), _admin: User = Depends(admin_auth)):
     return crud.update_filename(db, posture)
 
 
 @posture.put("/sensor", response_model=Posture, responses=error_responses([UnauthorizedException, ForbiddenException]))
-async def update_sensor(posture: PostureOnlysensor, db: Session = Depends(get_db), _admin: User = Depends(admin_auth)):
+async def update_sensor(posture: PostureOnlySensor, db: Session = Depends(get_db), _admin: User = Depends(admin_auth)):
     return crud.update_sensor(db, posture)
 
 
