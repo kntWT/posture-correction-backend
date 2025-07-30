fbead7e9c5d5673e5b95c97d310abbd728e8c5d9
kntWT <kento.wata0628@gmail.com>
2025-07-30 18:34:57 +0900

    fix(api): icon_url追加、updateのschema修正

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from schemas.common import CamelCaseModel
+from typing import Optional
 
 
 class User(CamelCaseModel):
@@ -7,19 +8,29 @@ class User(CamelCaseModel):
     name: str = ""
     password: str = ""
     email: str | None
+    icon_url: str | None = None
     token: str
     is_admin: bool = False
     standard_posture_id: int | None
     created_at: datetime
+    updated_at: datetime
 
 class UserWithoutToken(CamelCaseModel):
     id: int
-    name: str = ""
+    name: str
     password: str = ""
-    email: str | None
+    email: str | None = None
+    icon_url: str | None = None
     is_admin: bool = False
     standard_posture_id: int | None
     created_at: datetime
+    updated_at: datetime
+
+
+class UserUpdate(CamelCaseModel):
+    name: Optional[str] = None
+    password: Optional[str] = None
+    icon_url: Optional[str] = None
 
 
 class UserBasicAuth(CamelCaseModel):
@@ -40,6 +51,7 @@ class UserCreateBasic(CamelCaseModel):
 class UserCreateEmail(CamelCaseModel):
     email: str
     name: str
+    icon_url: str | None = None
     is_admin: bool = False
 
 
